12c73029ed8642396b2b093d26782d5f2d0122ce
Nova-022005 <sauravsingh8806@gmail.com>
2026-05-09 18:36:28 +0530

    mm

--- 01basic/src/App.js
+++ 01basic/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+ import Chai from './chai.js'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <h1>chai aur react </h1>
+     <Chai /> 
+    </>
+   
   );
 }
 
 export default App;
+
+//* the name of components  should be capatalized also in case of vitethe file name should be in jsx the convertion is to name file also in capitalize/camelcase
--- 01basic/src/index.js
+++ 01basic/src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//? DOM is like  a tree , React creates it's own DOM
+//? Which is known as Virtual DOM
+
+root.render(
+  <>
+    <App /> 
+    
+  </>
+);
+//! react give you power to render html by js
+
+//* React script  automatically load index.js in index.html and other scripts 
+//? react-scripts automatically bundles and loads index.js into index.html, along with other scripts, so we don’t need to add a <script> tag manually.
