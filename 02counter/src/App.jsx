99d6113b2ee9cad897c10f2f52aededab1ef8856
Nova-022005 <sauravsingh8806@gmail.com>
2026-06-27 11:49:09 +0530

    refactor: improve counter logic and formatting in App component

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -3,32 +3,35 @@ import './App.css'
 
 function App() {
 
-  const [counter,setCounter]=useState(15)
+  const [counter, setCounter] = useState(15)
   // let counter =15
 
-  const addValue=()=>{
-    setCounter(counter+1)
-    console.log('clicked',counter);
-    
+  const addValue = () => {
+    setCounter(prevCount => prevCount + 1)
+    setCounter(prevCount => prevCount + 1)
+    setCounter(prevCount => prevCount + 1)
+    setCounter(prevCount => prevCount + 1)
+    console.log('clicked', counter);
+
   }
-  const removeValue=()=>{
-    setCounter(counter-1)
-    console.log('clicked',counter);
-    
+  const removeValue = () => {
+    setCounter(counter - 1)
+    console.log('clicked', counter);
+
   }
   return (
     <>
-    <h1>chai aur react</h1>
-    <h2>Counter value: {counter}</h2>
-    
-    <button
-    onClick={addValue}
-    >Add value</button>
+      <h1>chai aur react</h1>
+      <h2>Counter value: {counter}</h2>
 
-    <button
-    onClick={
-      removeValue
-    }>Remove value</button>
+      <button
+        onClick={addValue}
+      >Add value</button>
+
+      <button
+        onClick={
+          removeValue
+        }>Remove value</button>
     </>
   )
 }
