80e4f038e70902a5d862b18a6ec1d1e9acd2191c
Nova-022005 <sauravsingh8806@gmail.com>
2026-08-30 18:27:48 +0530

    reactRouter

---
+++ Project/colorBackground/src/Currency.jsx
@@ -0,0 +1,13 @@
+import {use,useSate} from 'react'
+
+function Currency(){
+  
+  return(
+    <>
+    <h1 className='text-xl bg-orange-300 rounded text-center '>Currency App</h1>
+
+    </>
+  );
+}
+
+export default Currency
